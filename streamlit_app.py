362e24da4e7d7856fcc9f43003573c52cdf73e8e
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-07-01 20:24:28 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ def resource2():
 pages = {
     "Main" : [
         st.Page("home.py", title="Home"),
-        st.Page(page2(), title="Page 2")
+        st.Page("about.py", title="About")
     ],
     "Resources" : [
         st.Page(resource1(), title="Resource 1"),
