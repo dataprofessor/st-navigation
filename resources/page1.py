5b7a3353f04391d94c1738f38ae7db63b39f2508
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-07-03 13:55:28 -0700

    Update page1.py

--- resources/page1.py
+++ resources/page1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.header("This is Resources Page 1")
+st.subheader("This is Resources Page 1")
 
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 st.area_chart(chart_data)
