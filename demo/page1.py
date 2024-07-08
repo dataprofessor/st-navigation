3087749897d0d3b06457f9cea51c9d2eb93ef6a4
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-07-07 22:35:56 -0700

    Update page1.py

--- demo/page1.py
+++ demo/page1.py
@@ -4,7 +4,7 @@ import numpy as np
 
 st.subheader("Demo 1")
 
-st.write("Draw a chart using the PyDeck library via the `st.pydeck_chart()` method.")
+st.write("Display an area chart using the `st.area_chart()` method.")
 
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 st.area_chart(chart_data)
