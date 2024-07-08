22191194c123be3eed2c8f1884476757fa63bf2c
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-07-07 22:39:12 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,27 @@ st.set_page_config(page_title="Ex-stream-ly Cool App", page_icon="🧊")
 st.title('🧊 Ex-stream-ly Cool App')
 
 def page3():
-    st.write("This is **Demo 3**.")
+    import pandas as pd
+    import numpy as np
     
+    st.write("Display a chart using the Vega-Lite library via the `st.vega_lite_chart()` method.")
+
+    chart_data = pd.DataFrame(np.random.randn(200, 3), columns=["a", "b", "c"])
+
+    st.vega_lite_chart(
+       chart_data,
+       {
+           "mark": {"type": "circle", "tooltip": True},
+           "encoding": {
+               "x": {"field": "a", "type": "quantitative"},
+               "y": {"field": "b", "type": "quantitative"},
+               "size": {"field": "c", "type": "quantitative"},
+               "color": {"field": "c", "type": "quantitative"},
+           },
+       },
+    )
+
+
 pages = {
     "Main" : [
         st.Page("home.py", title="Home", icon=":material/home:"),
