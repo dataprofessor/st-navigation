2186c42f710790b6a515c15dab2c91d0840523e7
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-07-08 10:28:50 -0700

    Update about.py

--- about.py
+++ about.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.write("This is the **About** page.")
+st.write("""
+  Demo examples are from the [Streamlit Documentation](https://docs.streamlit.io/):
+    - [st.area_chart](https://docs.streamlit.io/develop/api-reference/charts/st.area_chart)
+    - [st.pydeck_chart](https://docs.streamlit.io/develop/api-reference/charts/st.pydeck_chart)
+    - [st.vega_lite_chart](https://docs.streamlit.io/develop/api-reference/charts/st.vega_lite_chart)
+""")
