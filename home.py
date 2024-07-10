7e438c0c358fa8177fd2a714597e7bd081ba3e43
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-07-09 19:57:02 -0700

    Update home.py

--- home.py
+++ home.py
@@ -6,6 +6,6 @@ st.write("""
   `st.navigation()` creates a navigation menu in the sidebar to provide access to available pages of this multi-page app.
 
   In a nutshell, you'll need to:
-  - Create an entrypoint file that acts as a router to direct users to specific pages of interest (`streamlit_app.py` in this demo)
+  - Create an **entrypoint** file that acts as a router to direct users to specific pages of interest (`streamlit_app.py` in this demo)
 
 """)
