f5f268eea8d609f9846708a7afd3c5520c56227d
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-07-09 20:38:38 -0700

    Update home.py

--- home.py
+++ home.py
@@ -5,11 +5,13 @@ st.info("Demo page for the `st.navigation()` method.")
 st.write("""
   `st.navigation()` creates a navigation menu in the sidebar to provide access to available pages of a multi-page app.
 
-  In a nutshell, you'll need to:
-  - Create an **entrypoint** file (`streamlit_app.py` in this demo) that acts as a router to direct users to specific pages of interest.
-  - This **entrypoint** file makes use of `st.navigation()` to define the page hierarchy in a multi-page app (previously not possible in a multi-page app)
-  - Each **page** is defined using `st.Page()` that could either be callable functions or Python `.py` files.
-  - Streamlit executes the entrypoint file with every app rerun where the `.run()` method is applied on the page object returned by `st.navigation()`.
+  To put it simply, follow these steps:
+
+  1. **Create an entrypoint file** (`streamlit_app.py` in this example). This file will serve as a router, directing users to specific pages of interest.
+  2. **Use `st.navigation()`** in the entrypoint file to define the page hierarchy for a multi-page app. This functionality was previously unavailable in multi-page apps.
+  3. **Define each page** using `st.Page()`. Pages can be callable functions or Python `.py` files.
+  4. **Streamlit runs the entrypoint file** on every app rerun, applying the `.run()` method to the page object returned by `st.navigation()`.
+
 """)
 
 st.page_link("https://docs.streamlit.io/develop/api-reference/navigation/st.navigation", label="Streamlit Docs on st.navigation", icon="📖")
