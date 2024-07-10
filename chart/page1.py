cf31baf02754b8ffdbe3d554a616657b88feaf05
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-07-10 16:45:19 -0700

    Update page1.py

--- chart/page1.py
+++ chart/page1.py
@@ -6,5 +6,6 @@ st.subheader("st.area_chart")
 
 st.write("Display an area chart using the `st.area_chart()` method.")
 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
-st.area_chart(chart_data)
+with st.echo():
+  chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+  st.area_chart(chart_data)
