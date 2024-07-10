97f8b87ae8e5be6cf6f098bdb59f1f2f3b158299
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-07-09 20:19:39 -0700

    Update home.py

--- home.py
+++ home.py
@@ -7,8 +7,8 @@ st.write("""
 
   In a nutshell, you'll need to:
   - Create an **entrypoint** file (`streamlit_app.py` in this demo) that acts as a router to direct users to specific pages of interest.
-  - This **entrypoint** file makes use of `st.navigation()` to define the hierarchy of **page elements** in the multi-page app.
-  - Each **page elements** is defined using `st.Page()` that could either be callable functions or Python `.py` files.
+  - This **entrypoint** file makes use of `st.navigation()` to define the page hierarchy in a multi-page app.
+  - Each **page** is defined using `st.Page()` that could either be callable functions or Python `.py` files.
 """)
 
 st.page_link("https://docs.streamlit.io/develop/api-reference/navigation/st.navigation", label="Streamlit Docs on st.navigation", icon="📖")
